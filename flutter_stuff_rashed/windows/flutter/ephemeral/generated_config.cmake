598c7d5cf42301cd1a083aa7a360abb88a9599ad
RachedArda <rashedalarda1990@gmail.com>
2025-11-30 14:11:28 -0500

    ts

---
+++ flutter_stuff_rashed/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\rashe\\OneDrive\\Desktop\\SD\\New folder\\Senior-Design-2025\\flutter_stuff_rashed" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter"
+  "PROJECT_DIR=C:\\Users\\rashe\\OneDrive\\Desktop\\SD\\New folder\\Senior-Design-2025\\flutter_stuff_rashed"
+  "FLUTTER_ROOT=D:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\rashe\\OneDrive\\Desktop\\SD\\New folder\\Senior-Design-2025\\flutter_stuff_rashed\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\rashe\\OneDrive\\Desktop\\SD\\New folder\\Senior-Design-2025\\flutter_stuff_rashed"
+  "FLUTTER_TARGET=C:\\Users\\rashe\\OneDrive\\Desktop\\SD\\New folder\\Senior-Design-2025\\flutter_stuff_rashed\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZDY5M2I0YjlkYg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YzI5ODA5MTM1MQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\rashe\\OneDrive\\Desktop\\SD\\New folder\\Senior-Design-2025\\flutter_stuff_rashed\\.dart_tool\\package_config.json"
+)
